291c4aac5bc21596da4995b3bf3176a0c76e98f6
polux <polux@google.com>
2012-10-10 21:44:48 +0200

    first commit

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+OUTDIR=$ROOTDIR/continuous
+TMPDIR=`mktemp -d`
+
+cd $TMPDIR
+git clone https://code.google.com/p/dart-immutable/
+cd dart-immutable
+pub install
+rm -rf $OUTDIR
+dartdoc -v --generate-app-cache --link-api --pkg=packages/ --out $OUTDIR lib/dart_immutable.dart
+rm -rf $TMPDIR
