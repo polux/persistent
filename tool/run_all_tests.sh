afbdff6e67d09c18b389e1c0b2186216461bc579
Paul Brauner <polux@google.com>
2013-05-29 12:25:12 +0200

    use new analyzer in tests

--- tool/run_all_tests.sh
+++ tool/run_all_tests.sh
@@ -2,15 +2,8 @@
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
-results=`dart_analyzer --work=/tmp $ROOT_DIR/lib/*.dart 2>&1`
-if [ -n "$results" ]; then
-    echo "$results"
-    exit 1
-else
-    echo "done"
-fi
-
-dart --enable-checked-mode $ROOT_DIR/example/map_example.dart \
+dart_analyzer $ROOT_DIR/lib/*.dart \
+&& dart --enable-checked-mode $ROOT_DIR/example/map_example.dart \
 && dart --enable-checked-mode $ROOT_DIR/example/set_example.dart \
 && dart --enable-checked-mode $ROOT_DIR/test/option_test.dart \
 && dart $ROOT_DIR/test/map_test.dart --quiet --quickCheckMaxSize=300 --smallCheckDepth=7 \
