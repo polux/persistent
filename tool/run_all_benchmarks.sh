6c62f8a491dcaa6ead150721b46ce1032ff56f05
Anthony Lai <anthony@mixbook.com>
2014-07-31 01:06:47 -0700

    Escape double quotes in benchmark script and benchmark csv

--- tool/run_all_benchmarks.sh
+++ tool/run_all_benchmarks.sh
@@ -7,5 +7,5 @@ GIT_HASH=`git rev-parse HEAD`
 DATE=`date -u`
 SCORE=`dart $ROOT_DIR/benchmark/map_bench_wordcount.dart`
 
-echo "$DART_VERSION, $GIT_HASH, $DATE, $SCORE" >> $ROOT_DIR/benchmark/map_bench_wordcount.csv
+echo "\"$( echo $DART_VERSION | sed "s/\"/\"\"/g" )\", $GIT_HASH, $DATE, $SCORE" >> $ROOT_DIR/benchmark/map_bench_wordcount.csv
 
