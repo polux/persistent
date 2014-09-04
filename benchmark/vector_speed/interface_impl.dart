c9c13a532001f3c949177a9e589e3094fd9ee722
Sysel <marian.sysel.hornak@gmail.com>
2014-09-04 15:22:13 +0200

    Benchmark vector speed

---
+++ benchmark/vector_speed/interface_impl.dart
@@ -0,0 +1,65 @@
+part of map_bench;
+
+class PersistentVectorInterface<E>
+  extends EncapsulatingInterface<E, PersistentVector<E>>{
+    
+  create() => object = new PersistentVector<E>();
+  
+  push(E value) =>
+    object = object.push(value);
+  
+  get(int index) => object.get(index);
+    
+  set(int index, E value) => object = object.set(index, value);
+    
+  pop() => object = object.pop();
+  
+  _copy() => object;
+}
+
+  
+class TransientVectorInterface<E>
+  extends EncapsulatingInterface<E, TransientVector<E>>{
+    
+  create() => object = new PersistentVector<E>().asTransient();
+  
+  push(E value) =>
+    object.doPush(value);
+  
+  get(int index) => object.get(index);
+    
+  set(int index, E value) => object.doSet(index, value);
+    
+  pop() => object.doPop();
+  
+  _copy(){
+    return new PersistentVector.from(object).asTransient();
+  }
+}
+
+    
+class ListInterface<E>
+  extends EncapsulatingInterface<E, List<E>>{
+    
+  create() => object = [];
+  
+  push(E value) =>
+    object.add(value);
+  
+  get(int index) => object[index];
+    
+  set(int index, E value) => object[index] =  value;
+    
+  pop() => object.removeLast();
+      
+  _copy(){
+    return new List.from(object);
+  }
+}
+
+      
+
+
+  
+
+       
---
+++ benchmark/vector_speed/vector_speed.dart
@@ -0,0 +1,29 @@
+// Copyright (c) 2012, Google Inc. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+// Author: Paul Brauner (polux@google.com)
+
+library map_bench;
+
+import 'package:persistent/persistent.dart';
+import 'package:benchmark_harness/benchmark_harness.dart';
+
+part 'benchmark_overall.dart';
+part 'interface.dart';
+part 'interface_impl.dart';
+
+var interfaces = {
+  "PersistentVector": () => new PersistentVectorInterface(),
+  "TransientVector": () => new TransientVectorInterface(),
+  "List": () => new ListInterface(),
+};
+
+void main() {
+
+  for (int n in [1,10,100,1000,10000]) {
+    for (String name in interfaces.keys){
+      new OverallBenchmark(n, interfaces[name](), name).report();
+    }
+  }
+
+}
