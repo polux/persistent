0eda354bc1badbc48ca212ed43ff49e935287136
Sysel <marian.sysel.hornak@gmail.com>
2014-09-05 12:20:50 +0200

    Updated license

--- benchmark/vector_speed/benchmark_overall.dart
+++ benchmark/vector_speed/benchmark_overall.dart
@@ -1,7 +1,8 @@
+// Copyright (c) 2014, VaccumLabs.
 // Copyright (c) 2012, Google Inc. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-// Author: Paul Brauner (polux@google.com)
+// Authors are listed in the AUTHORS file
 
 part of vector_speed;
 
@@ -28,15 +29,15 @@ class OverallBenchmark extends BenchmarkBase{
     for (int i = size-1; i >= 0; i--) {
       object.get(i);
     }
-    
+
     for (int i = size-1; i >= 0; i--) {
       object.set(i, "${i}".padLeft(8,"_"));
     }
-    
+
     for (int i = 0; i < size; i++) {
       object.get(i);
     }
-    
+
     for (int i = 0; i < size; i++) {
       object.set(i, "${i}".padLeft(8,"x"));
     }
