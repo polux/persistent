32da21fbde3e2202894f9a56e0c6c35d27272fe9
Sysel <marian.sysel.hornak@gmail.com>
2014-09-25 15:22:08 +0200

    Added linked list tests to all

--- test/all.dart
+++ test/all.dart
@@ -4,6 +4,7 @@
 
 // Authors are listed in the AUTHORS file
 
+import 'linked_list_test.dart' as linked_list_test;
 import 'map_test.dart' as map_test;
 import 'set_test.dart' as set_test;
 import 'vector_test.dart' as vector_test;
@@ -11,6 +12,7 @@ import 'randomized_map_test.dart' as randomized_map_test;
 import 'randomized_vector_test.dart' as randomized_vector_test;
 
 main() {
+  linked_list_test.run();
   map_test.run();
   set_test.run();
   vector_test.run();
