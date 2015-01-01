574b6df5e19255f4fa4f3f806d7e3fc7fe6c7f0e
mama <tomas.kulich@gmail.com>
2015-01-01 20:29:31 +0100

    map speed benchmark refactored

--- benchmark/map_speed/benchmarks.dart
+++ benchmark/map_speed/benchmarks.dart
@@ -8,29 +8,31 @@ part of map_bench;
 
 class WriteBenchmark extends BenchmarkBase{
 
-  final int size;
-  final BenchmarkInterface object;
+  final Map<num, num> sample;
+  BenchmarkInterface object;
+  final dynamic factory;
 
+  void setup(){
+    object = factory();
+  }
 
-  WriteBenchmark(size, object, name):
-    size = size,
-    object = object,
-    super("Writing $name($size)");
-
+  WriteBenchmark(this.sample, this.factory):super('Writing');
 
   void run(){
-
-    object.create();
-
-    for (int i = 0; i < size; i++) {
-      object.assoc("key$i", "foo");
-      object.assoc("key$i", "bar");
-      object.assoc("key$i", "baz");
-      object.assoc("key$i", "woo");
-    }
-
-    for (int i = 0; i < size; i++) {
-      object.delete("key$i");
+    for (var size in this.sample.keys) {
+      for (var j=0; j<this.sample[size]; j++){
+        for (int i = 0; i < size; i++) {
+          var key = "key$i";
+          object.assoc("key$i", "foo");
+          object.assoc("key$i", "bar");
+          object.assoc("key$i", "baz");
+          object.assoc("key$i", "woo");
+        }
+        for (int i = 0; i < size; i++) {
+          var key = "key$i";
+          object.delete(key);
+        }
+      }
     }
   }
 }
@@ -38,32 +40,33 @@ class WriteBenchmark extends BenchmarkBase{
 
 class ReadBenchmark extends BenchmarkBase{
 
-  final int size;
-  final BenchmarkInterface object;
+  final Map<num, num> sample;
+  Map<num, BenchmarkInterface> objects = new Map();
+  final dynamic factory;
 
-
-  ReadBenchmark(size, object, name):
-    size = size,
-    object = object,
-    super("Reading $name($size)");
+  ReadBenchmark(this.sample, this.factory):super('Reading');
 
   void setup(){
-
-    object.create();
-
-    for (int i = 0; i < size; i++) {
-      object.assoc("key$i", "foo");
-    }
+    this.sample.forEach((size, count){
+      BenchmarkInterface object = factory();
+      objects[size] = object;
+      for (int i = 0; i < size; i++) {
+        object.assoc("key$i", "foo");
+      }
+    });
   }
 
   void run(){
-
-    for (int i = size * 2; i >= 0; i--) {
-      object.get("key$i");
-    }
-
-    for (int i = 0; i <= size * 2; i++) {
-      object.get("key$i");
+    for (var size in this.sample.keys) {
+      BenchmarkInterface object = objects[size];
+      for (var j=0; j<this.sample[size]; j++){
+        for (int i = size; i >= 0; i--) {
+          object.get("key$i");
+        }
+        for (int i = 0; i <= size; i++) {
+          object.get("key$i");
+        }
+      }
     }
   }
 }
--- benchmark/map_speed/map_speed.dart
+++ benchmark/map_speed/map_speed.dart
@@ -8,6 +8,7 @@ library map_bench;
 
 import 'package:vacuum_persistent/persistent.dart';
 import 'package:benchmark_harness/benchmark_harness.dart';
+import 'dart:math';
 
 part 'benchmarks.dart';
 part 'interface.dart';
@@ -19,30 +20,42 @@ var interfaces = {
   "Map": () => new StandardMapInterface(),
 };
 
-//var sizes = [2,10,10000];
-var sizes = [10000];
+
+preciseBenchmark(result, times){
+  var res = 0;
+  var ressqr = 0;
+  for (int i=0; i<times; i++){
+    var _res = result();
+    res += _res;
+    ressqr += _res * _res;
+  }
+  res /= times;
+  ressqr /= times;
+  return [res, sqrt(ressqr-res*res)];
+}
+
+var sizes = [{1000:6, 2000: 3, 3000: 2, 6000: 1}];
+int times = 10;
 
 void main() {
-//  for (int n in sizes) {
-//    double unit;
-//    unit = new ReadBenchmark(n, interfaces["Map"](), '').measure();
-//    unit = new ReadBenchmark(n, interfaces["Map"](), '').measure();
-//    for (String name in interfaces.keys){
-//      double res = new ReadBenchmark(n, interfaces[name](), name).measure();
-//      print('Reading ${name} size $n: ${res/unit} ${res} us');
-//    }
-//    print('');
-//  }
-//  print('');
-  for (int n in sizes) {
-    double unit;
-    unit = new WriteBenchmark(n, interfaces["Map"](), '').measure();
-    unit = new WriteBenchmark(n, interfaces["Map"](), '').measure();
-    for (String name in interfaces.keys){
-      double res = new WriteBenchmark(n, interfaces[name](), name).measure();
-      print('Writing ${name} size $n: ${res/unit} ${res} us');
+  var config =
+  {'Write': ((sample, factory) => (new WriteBenchmark(sample, factory))),
+   'Read': ((sample, factory) => (new ReadBenchmark(sample, factory))),
+  };
+  config.forEach((mode, creator){
+    for (Map sample in sizes) {
+      List<double> unit =
+        preciseBenchmark(() => creator(sample, interfaces['Map']).measure(),
+        times);
+      for (String name in interfaces.keys) {
+        List<double> res = preciseBenchmark(
+          () => creator(sample, interfaces[name]).measure(),
+          times);
+        print('${mode} ${name} sample ${sample}: ${res[0]/unit[0]} ${res[0]} us +- ${res[1]/res[0]*100} %');
+      }
+      print('');
     }
-    print('');
-  }
+  });
+
 
 }
